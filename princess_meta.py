0cf0ddf95e0c0cd595015d35362807296b90f7b4
yoPitarch <pitarch@irit.fr>
2016-04-18 20:34:19 +0200

    cross_validation_20h

--- princess_meta.py
+++ princess_meta.py
@@ -36,6 +36,7 @@ def generate_script():
 
     # nbExp = len(glob.glob('./Experiment*')) + 1
     # dirname = './osirim+sig/PROJET/PRINCESS/code/script_experiments/'
+    os.system("rm " + dirname + "/*")
     print dirname
     os.mkdir(dirname)
     with open(dirname + '/run.sh', 'w') as script_file:
@@ -53,15 +54,15 @@ def generate_script():
                                                               '-g:5-s:' + elStrategy + '-a-f:' + elFeature + '.sh'
                                             with open(dirname + '/' + sbatch_filename, 'w') as the_file:
                                                 the_file.write(
-                                                    "#!/bin/sh\n#SBATCH --job-name=sigir\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=pitarch@irit.fr\n#SBATCH --output=group.out\n#SBATCH --error=group.err\n")
+                                                    "#!/bin/sh\n#SBATCH --job-name=sigir\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=pitarch@irit.fr\n#SBATCH --output=group.out\n#SBATCH --error=group.err \n#SBATCH -n 64 \n ")
 
                                                 if elFeature == '':
                                                     the_file.write(
-                                                        'srun python /projets/sig/PROJET/PRINCESS/code/princess/princess.py -t ' + elType + ' -x -' + elFold + ' -r ' + elRound + ' -b 0.2 -c ' + elCollection + \
+                                                        'srun /logiciels/Python-2.7/bin/python2.7 /projets/sig/PROJET/PRINCESS/code/princess/princess.py -p 63 -t ' + elType + ' -x -' + elFold + ' -r ' + elRound + ' -b 0.2 -c ' + elCollection + \
                                                         ' -l ' + elLife + ' -i ' + elImpact + ' -g 5 -n 0 -s ' + elStrategy + "\n")
                                                 else:
                                                     the_file.write(
-                                                        'srun python /projets/sig/PROJET/PRINCESS/code/princess/princess.py -t ' + elType + ' -x -' + elFold + ' -r ' + elRound + ' -b 0.2 -c ' + elCollection + ' -l ' + elLife + ' -i ' + elImpact + ' -g 5 -n 0 -s ' + elStrategy + ' -a -f ' + elFeature + "\n")
+                                                        'srun /logiciels/Python-2.7/bin/python2.7 /projets/sig/PROJET/PRINCESS/code/princess/princess.py -p 63 -t ' + elType + ' -x -' + elFold + ' -r ' + elRound + ' -b 0.2 -c ' + elCollection + ' -l ' + elLife + ' -i ' + elImpact + ' -g 5 -n 0 -s ' + elStrategy + ' -a -f ' + elFeature + "\n")
 
                                             script_file.write("sbatch " + sbatch_filename + "\n")
                                         else:
@@ -69,13 +70,13 @@ def generate_script():
                                             sbatch_filename = 'osirim_battle-t:' + elType + '-x:' + elFold + '-r:' + elRound + '-b:0.2-c:' + elCollection + '-l:' + elLife + '-i:' + elImpact + '-g:5-s:' + elStrategy + '-a-f:' + elFeature + '.sh'
                                             with open(dirname + '/' + sbatch_filename, 'w') as the_file:
                                                 the_file.write(
-                                                    "#!/bin/sh\n#SBATCH --job-name=sigir\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=pitarch@irit.fr\n#SBATCH --output=group.out\n#SBATCH --error=group.err\n")
+                                                    "#!/bin/sh\n#SBATCH --job-name=sigir\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=pitarch@irit.fr\n#SBATCH --output=group.out\n#SBATCH --error=group.err\n#SBATCH -n 64 \n")
                                                 if elFeature == '':
                                                     the_file.write(
-                                                        'srun python /projets/sig/PROJET/PRINCESS/code/princess/princess.py -t ' + elType + ' -x -' + elFold + ' -r ' + elRound + ' -b 0.2 -c ' + elCollection + ' -l ' + elLife + ' -i ' + elImpact + ' -g 5 -n 0 -s ' + elStrategy + "\n")
+                                                        'srun /logiciels/Python-2.7/bin/python2.7 /projets/sig/PROJET/PRINCESS/code/princess/princess.py -p 63  -t ' + elType + ' -x -' + elFold + ' -r ' + elRound + ' -b 0.2 -c ' + elCollection + ' -l ' + elLife + ' -i ' + elImpact + ' -g 5 -n 0 -s ' + elStrategy + "\n")
                                                 else:
                                                     the_file.write(
-                                                        'srun python /projets/sig/PROJET/PRINCESS/code/princess/princess.py -t ' + elType + ' -x -' + elFold + ' -r ' + elRound + ' -b 0.2 -c ' + elCollection + ' -l ' + elLife + ' -i ' + elImpact + ' -g 5 -n 0 -s ' + elStrategy + ' -a -f ' + elFeature + "\n")
+                                                        'srun /logiciels/Python-2.7/bin/python2.7 /projets/sig/PROJET/PRINCESS/code/princess/princess.py -p 63 -t ' + elType + ' -x -' + elFold + ' -r ' + elRound + ' -b 0.2 -c ' + elCollection + ' -l ' + elLife + ' -i ' + elImpact + ' -g 5 -n 0 -s ' + elStrategy + ' -a -f ' + elFeature + "\n")
                                             script_file.write("sbatch " + sbatch_filename + "\n")
 
     print count
@@ -123,51 +124,31 @@ def extractMapXp(fold):
         # print " ".join(table)
         os.system(" ".join(table))
 
+        with open(outfilenameeval, 'r') as myFile:
+            for line in myFile:
+                if "all" in line:
+                    for i in re.finditer(regex, line):
+                        # feat = i.group(2)
+                        maps[fold][xp] = float(i.group(3))
 
-def writeScriptTest():
-    with open(outfilenameeval, 'r') as myFile:
-        for line in myFile:
-            if "all" in line:
-                for i in re.finditer(regex, line):
-                    # feat = i.group(2)
-                    maps[fold][xp] = float(i.group(3))
 
-                    if "group" in elType:
-                        count += 1
-                        sbatch_filename = 'osirim_battle-t:' + elType + '-x:' + elFold + '-r:' + elRound + '-b:0.2-c:' + elCollection + '-l:' + elLife + '-i:' + elImpact + \
-                                          '-g:5-s:' + elStrategy + '-a-f:' + elFeature + '.sh'
-                        with open(dirname + '/' + sbatch_filename, 'w') as the_file:
-                            the_file.write(
-                                "#!/bin/sh\n#SBATCH --job-name=sigir\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=pitarch@irit.fr\n#SBATCH --output=group.out\n#SBATCH --error=group.err\n")
+def runTest(fold, best):
+    idfold = fold.split("/")[-3]
+    command = "srun --mail-type=ALL --mail-user=pitarch@irit.fr --ntasks=64 --output=best.out " \
+              "--error=best.err --job-name=best /logiciels/Python-2.7/bin/python2.7 -x " + idfold
+    t = best.split("-")
+    for param in t:
+        tparam = param.split(":")
+        command += " " + tparam[0] + " " + tparam[1]
+    os.system(command)
 
-                            if elFeature == '':
-                                the_file.write(
-                                    'srun python /projets/sig/PROJET/PRINCESS/code/princess/princess.py -t ' + elType + ' -x -' + elFold + ' -r ' + elRound + ' -b 0.2 -c ' + elCollection + \
-                                    ' -l ' + elLife + ' -i ' + elImpact + ' -g 5 -n 0 -s ' + elStrategy + "\n")
-                            else:
-                                the_file.write(
-                                    'srun python /projets/sig/PROJET/PRINCESS/code/princess/princess.py -t ' + elType + ' -x -' + elFold + ' -r ' + elRound + ' -b 0.2 -c ' + elCollection + ' -l ' + elLife + ' -i ' + elImpact + ' -g 5 -n 0 -s ' + elStrategy + ' -a -f ' + elFeature + "\n")
-
-                        script_file.write("sbatch " + sbatch_filename + "\n")
-                    else:
-                        count += 1
-                        sbatch_filename = 'osirim_battle-t:' + elType + '-x:' + elFold + '-r:' + elRound + '-b:0.2-c:' + elCollection + '-l:' + elLife + '-i:' + elImpact + '-g:5-s:' + elStrategy + '-a-f:' + elFeature + '.sh'
-                        with open(dirname + '/' + sbatch_filename, 'w') as the_file:
-                            the_file.write(
-                                "#!/bin/sh\n#SBATCH --job-name=sigir\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=pitarch@irit.fr\n#SBATCH --output=group.out\n#SBATCH --error=group.err\n")
-                            if elFeature == '':
-                                the_file.write(
-                                    'srun python /projets/sig/PROJET/PRINCESS/code/princess/princess.py -t ' + elType + ' -x -' + elFold + ' -r ' + elRound + ' -b 0.2 -c ' + elCollection + ' -l ' + elLife + ' -i ' + elImpact + ' -g 5 -n 0 -s ' + elStrategy + "\n")
-                            else:
-                                the_file.write(
-                                    'srun python /projets/sig/PROJET/PRINCESS/code/princess/princess.py -t ' + elType + ' -x -' + elFold + ' -r ' + elRound + ' -b 0.2 -c ' + elCollection + ' -l ' + elLife + ' -i ' + elImpact + ' -g 5 -n 0 -s ' + elStrategy + ' -a -f ' + elFeature + "\n")
-                        script_file.write("sbatch " + sbatch_filename + "\n")
 
 
 def findBestConfig(fold):
     listResults = maps[fold]
     sorted_x = sorted(x.items(), key=operator.itemgetter(1), reverse=True)
     best = sorted_x[0]
+    return best
 
 
 
@@ -179,15 +160,19 @@ def findBestConfig(fold):
 # Expe generation
 generate_script()
 
+
 # Script execution
+os.system("scancel -u quaesig")
 os.system("chmod a+x " + dirname + "/run.sh")
 os.system("./" + dirname + "/run.sh")
 
 startTime = time.time()
 while get_running_jobs() > 0:
     if time.time() - startTime % 7200 == 0:
+        print "je checke"
         l = checkDoneXp()
         if len(l) > 0:
             for fold in l:
                 extractMapXp(fold)
-                findBestConfig(fold)
+                best = findBestConfig(fold)
+                runTest(fold, best)
