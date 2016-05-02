03c80fdff3d1b0662b3939e4e68bf69527d24860
yoPitarch <pitarch@irit.fr>
2016-05-02 17:39:09 +0200

    Add findBestResultAndRunExpe

--- game/__init__.py
+++ game/__init__.py
@@ -1,7 +1,8 @@
+from .tournament import Tournament
+from .match import Match
 from .group_stage_optim_cikm import GroupStageOptim
 from .group_swiss_optim_cikm import GroupSwissOptim
-from .match import Match
 from .random_tournament import RandomTournament
 from .round_robin_cikm import RoundRobin
 from .swiss_system_cikm import SwissSystem
-from .tournament import Tournament
+
