a82b9bc5655610651a0b0a347021605518eec8a1
yoPitarch <pitarch@irit.fr>
2016-05-12 14:12:37 +0200

    Add findBestResultAndRunExpe

---
+++ feature_extraction/compute_std_50.py
@@ -0,0 +1,56 @@
+import pprint
+
+import numpy as np
+from pymongo import MongoClient
+
+# ********************************************
+# CONNEXION DANS MONGODB
+# ********************************************
+normType = "max"
+maxRes = "50"
+tColName = ["indri_robust2004_", "indri_web2014clueweb12_adhoc_", "np2003_50", "np2004_50",
+            "ohsumed_50", "td2003_50", "td2004_50", "hp2003_50", "hp2004_50"]
+
+
+
+connection = MongoClient(host='co2-ni01.irit.fr',port=28018)
+db = connection.princess
+for colName in tColName :
+    if "indri" in colName:
+        collection_name = colName + normType + maxRes
+    else:
+        collection_name = colName
+    collection = db[collection_name]
+    queries = collection.distinct('query')
+    result_collection = db[collection_name + "_std"]
+    result_collection.remove({})
+    for q in queries:
+        toInsert = {}
+        toInsert["query"] = q
+        toInsert["stds"] = {}
+        valFeatures = {}
+        stdFeatures = {}
+        # la requete qui liste tous les documents
+        list = collection.find({'query': q}, {'_id': 0, 'docs': 1})
+        count = 0
+        list_doc = []
+        for d in list[0]['docs']:
+            # print "**********"
+            count += 1
+            # name = d['doc_name']
+
+            list_feat = {}
+            for f in d['features']:
+                toInsert["stds"].setdefault(f, {})
+                valFeatures.setdefault(f, [])
+                valFeatures[f].append(d['features'][f])
+
+        for feat in valFeatures:
+            std = np.std(np.array(valFeatures[feat]))
+            toInsert["stds"][feat] = std
+            print valFeatures[feat]
+            print "std"
+            print std
+
+        data = result_collection.save(toInsert)
+        pprint.pprint(data)
